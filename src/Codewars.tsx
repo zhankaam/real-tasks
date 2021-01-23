2dffce1898f33aa260c9fbfa330a318292c598fb
zhankaam <zhankaam@gmail.com>
2021-01-23 21:08:14 +0300

    codewars tasks

---
+++ src/Codewars.tsx
@@ -0,0 +1,114 @@
+export function Codewars(props: any) {
+    return <div>
+      {/*  <h3>Task 1</h3>
+        function helloWorld() {
+        const str = "Hello World!"
+        console.log(str)
+    }
+
+        helloWorld = function() {
+        let string = "Hello World!";
+        console.log( str );
+    }*/}
+
+    {/*<h3>Task 2 </h3>
+        const v1 = 50
+        const v2 = 100
+        const v3 = 150
+        const v4 = 200
+        const v5 = 2
+        const v6 = 250
+
+        const equal1 = () => v1 + v1
+        const equal2 = () => v4 - v2
+        const equal3 = () => v1 * v5
+        const equal4 = () => v4 / v5
+        const equal5 = () => v6 % v3*/}
+
+        {/*<h3>Task 3 </h3>
+        let a1="A",a2="a",b1="B",b2="b",c1="C",c2="c",d1="D",d2="d",e1="E",e2="e",n1="N",n2="n"
+        function Dad(){
+        //select some variable to combine "Dad"
+        return d1+a2+d2;
+    }
+        function Bee(){
+        //select some variable to combine "Bee"
+        return b1+e2+e2;
+    }
+        function banana(){
+        //select some variable to combine "banana"
+        return b2+a2+n2+a2+n2+a2;
+    }
+
+        //answer some questions if you finished works above
+        function answer1(){
+        //the answer should be "yes" or "no"
+        return "no";
+    }
+        function answer2(){
+        //the answer should be "yes" or "no"
+        return "no";
+    }
+        function answer3(){
+        //the answer should be "yes" or "no"
+        return "yes";
+    }*/}
+
+    {/*<h3>Task 4</h3>
+        function getLength(arr){
+        //return length of arr
+        return arr.length;
+    }
+        function getFirst(arr){
+        //return the first element of arr
+        return arr[0];
+    }
+        function getLast(arr){
+        //return the last element of arr
+        return arr[arr.length - 1];
+    }
+        function pushElement(arr){
+        var el=1;
+        //push el to arr
+        arr.push(el)
+        return arr;
+    }
+        function popElement(arr){
+        //pop an element from arr
+        arr.pop();
+        return arr;
+    }
+*/}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    </div>
+}
+
+
+
+
+
+
+
+
+
+
+
